37a90430088c677a3c5b0a0d6cdab847fb8edee6
Sru-12 <161029364+Sru-12@users.noreply.github.com>
2024-03-22 10:33:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,147 @@
+const audio1 = new Audio("A 1 Malang Sajna.mp3");
+document.querySelector("btn1").addEventListener("click",function () {
+    audio1.play();
+});
+document.querySelector("btn2").addEventListener("click",function () {
+    audio1.pause();
+});
+const audio2 = new Audio("A ");
+document.querySelector("btn3").addEventListener("click",function () {
+    audio2.play();
+});
+document.querySelector("btn4").addEventListener("click",function () {
+    audio2.pause();
+});
+const audio3 = new Audio("A 3 Rattan_Lamiyan.mp3 Rattan");
+document.querySelector("btn5").addEventListener("click",function () {
+    audio3.play();
+});
+document.querySelector("btn6").addEventListener("click",function () {
+    audio3.pause();
+});
+const audio4 = new Audio("A 4 love u zindagi.mp3");
+document.querySelector("btn7").addEventListener("click",function () {
+    audio4.play();
+});
+document.querySelector("btn8").addEventListener("click",function () {
+    audio4.pause();
+});
+const audio5 = new Audio("A 5 Sukh Kalale.mp3");
+document.querySelector("btn9").addEventListener("click",function () {
+    audio5.play();
+});
+document.querySelector("btn10").addEventListener("click",function () {
+    audio5.pause();
+});
+const audio6 = new Audio("A 6 Heer Ranjha.mp3");
+document.querySelector("btn11").addEventListener("click",function () {
+    audio6.play();
+});
+document.querySelector("btn12").addEventListener("click",function () {
+    audio6.pause();
+});
+const audio7 = new Audio("A 7Samjhawan.mp3");
+document.querySelector("btn13").addEventListener("click",function () {
+    audio7.play();
+});
+document.querySelector("btn14").addEventListener("click",function () {
+    audio7.pause();
+});
+const audio8 = new Audio("A 8 leja re.mp3");
+document.querySelector("btn15").addEventListener("click",function () {
+    audio8.play();
+});
+document.querySelector("btn16").addEventListener("click",function () {
+    audio8.pause();
+});
+const audio9 = new Audio("A 9radha rani.mp3");
+document.querySelector("btn17").addEventListener("click",function () {
+    audio9.play();
+});
+document.querySelector("btn18").addEventListener("click",function () {
+    audio9.pause();
+});
+const audio10 = new Audio("A 10 rajhana.mp3");
+document.querySelector("btn19").addEventListener("click",function () {
+    audio10.play();
+});
+document.querySelector("btn20").addEventListener("click",function () {
+    audio10.pause();
+});
+const audio11 = new Audio("B 1 Rutho-Jo.mp3");
+document.querySelector("btn21").addEventListener("click",function () {
+    audio11.play();
+});;
+document.querySelector("btn22").addEventListener("click",function () {
+    audio11.pause();
+});
+const audio12 = new Audio("B 2 Bappa-Wala.mp3");
+document.querySelector("btn23").addEventListener("click",function () {
+    audio12.play();
+});
+document.querySelector("btn24").addEventListener("click",function () {
+    audio12.pause();
+});
+const audio13 = new Audio("B 3 Mere Ghar Ram Aaye Hain.mp3");
+document.querySelector("btn25").addEventListener("click",function () {
+    audio13.play();
+});
+document.querySelector("btn26").addEventListener("click",function () {
+    audio13.pause();
+});
+const audio14 = new Audio("B 4 Bholenath Ki Shadi.mp3");
+document.querySelector("btn27").addEventListener("click",function () {
+    audio14.play();
+});
+document.querySelector("btn28").addEventListener("click",function () {
+    audio14.pause();
+});
+const audio15 = new Audio("B 5 Rakhumaai.mp3");
+document.querySelector("btn29").addEventListener("click",function () {
+    audio15.play();
+});
+document.querySelector("btn30").addEventListener("click",function () {
+    audio15.pause();
+});
+const audio16 = new Audio("C 1 maan.mp3");
+document.querySelector("btn31").addEventListener("click",function () {
+    audio16.play();
+});
+document.querySelector("btn32").addEventListener("click",function () {
+    audio16.pause();
+});
+const audio17 = new Audio("C 2 Gulabi Saree.mp3");
+document.querySelector("btn33").addEventListener("click",function () {
+    audio17.play();
+});
+document.querySelector("btn34").addEventListener("click",function () {
+    audio17.pause();
+});
+const audio18 = new Audio("C 3 Mi-Naad.mp3");
+document.querySelector("btn35").addEventListener("click",function () {
+    audio18.play();
+});
+document.querySelector("btn36").addEventListener("click",function () {
+    audio18.pause();
+});
+const audio19 = new Audio("C 4 Tola-ToLA.mp3");
+document.querySelector("btn37").addEventListener("click",function () {
+    audio19.play();
+});
+document.querySelector("btn38").addEventListener("click",function () {
+    audio19.pause();
+});
+const audio20 = new Audio("C 5 mittawa.mp3");
+document.querySelector("btn39").addEventListener("click",function () {
+    audio20.play();
+});
+document.querySelector("btn40").addEventListener("click",function () {
+    audio20.pause();
+});
+const audio21 = new Audio("C 6 Navari-Sadi.mp3");
+document.querySelector("btn41").addEventListener("click",function () {
+    audio21.play();
+});
+document.querySelector("btn42").addEventListener("click",function () {
+    audio21.pause();
+});
